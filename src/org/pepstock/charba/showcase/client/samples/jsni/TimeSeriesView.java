6ae70fe222c74614f85e5fbcae20c4758af7bef6
stock <stocki.nail@gmail.com>
2019-01-10 12:22:35 +0100

    updated timeline

--- src/org/pepstock/charba/showcase/client/samples/jsni/TimeSeriesView.java
+++ src/org/pepstock/charba/showcase/client/samples/jsni/TimeSeriesView.java
@@ -17,11 +17,14 @@ import org.pepstock.charba.client.enums.TimeUnit;
 import org.pepstock.charba.client.events.DatasetRangeSelectionEvent;
 import org.pepstock.charba.client.events.DatasetRangeSelectionEventHandler;
 import org.pepstock.charba.client.items.TooltipItem;
+import org.pepstock.charba.client.options.scales.CartesianLinearAxis;
 import org.pepstock.charba.client.options.scales.CartesianTimeAxis;
 import org.pepstock.charba.client.plugins.InvalidPluginIdException;
 import org.pepstock.charba.client.plugins.impl.DatasetsItemsSelector;
+import org.pepstock.charba.client.plugins.impl.DatasetsItemsSelectorOptions;
 import org.pepstock.charba.showcase.client.samples.Colors;
 import org.pepstock.charba.showcase.client.samples.Toast;
+import org.pepstock.charba.showcase.client.samples.jsinterop.BaseComposite;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -113,7 +116,7 @@ public class TimeSeriesView extends BaseComposite{
 			dp1[i].setT(new Date(time));
 			time = time + DAY;
 		}
-		dataset1.setDataPoints(dp1);
+//		dataset1.setDataPoints(dp1);
 		dataset2.setDataPoints(dp1);
 		
 		final CartesianTimeAxis axis = new CartesianTimeAxis(chart);
@@ -121,11 +124,15 @@ public class TimeSeriesView extends BaseComposite{
 		axis.getTicks().setSource(TickSource.data);
 		axis.getTime().setUnit(TimeUnit.day);
 
-	    
+		CartesianLinearAxis axis2 = new CartesianLinearAxis(chart);
+		axis2.setDisplay(true);
+		axis2.getTicks().setBeginAtZero(true);
+
 		
-		chart.getData().setDatasets(dataset1);
+//		chart.getData().setDatasets(dataset1);
 		
 		chart.getOptions().getScales().setXAxes(axis);
+		chart.getOptions().getScales().setYAxes(axis2);
 		
 		small.getOptions().setResponsive(true);
 //		small.getOptions().setMaintainAspectRatio(true);
@@ -145,7 +152,11 @@ public class TimeSeriesView extends BaseComposite{
 		
 		small.getOptions().getScales().setXAxes(axis1Small);
 		
+		DatasetsItemsSelectorOptions pOptions = new DatasetsItemsSelectorOptions();
+		pOptions.setBorderWidth(1);
+		pOptions.setBorderDash(6);
 		try {
+			small.getOptions().getPlugins().setOptions(DatasetsItemsSelector.ID, pOptions.getObject());
 			small.getPlugins().add(new DatasetsItemsSelector());
 		} catch (InvalidPluginIdException e) {
 			// TODO Auto-generated catch block
@@ -160,12 +171,20 @@ public class TimeSeriesView extends BaseComposite{
 				sb.append("Dataset from: <b>").append(event.getFrom()).append("</b><br>");
 				sb.append("Dataset to: <b>").append(event.getTo()).append("</b><br>");
 				new Toast("Dataset Range Selected!", sb.toString()).show();
-				List<DataPoint> points = dataset1.getDataPoints();
-				Date min = points.get(event.getFrom()).getT();
-				Date max = points.get(event.getTo()).getT();
-			    axis.getTime().setMin(min);
-			    axis.getTime().setMax(max);
-			    chart.draw();
+//				List<DataPoint> points = dataset1.getDataPoints();
+				int tot = event.getTo() - event.getFrom() + 1;
+				DataPoint[] dp1 = new DataPoint[tot];
+				for (int i=0; i<tot; i++) {
+					dp1[i] = dataset2.getDataPoints().get(i+event.getFrom());
+					
+				}
+				dataset1.setDataPoints(dp1);
+				chart.getData().setDatasets(dataset1);
+//				Date min = points.get().getT();
+//				Date max = points.get(event.getTo()).getT();
+//			    axis.getTime().setMin(min);
+//			    axis.getTime().setMax(max);
+			    chart.update();
 
 			}
 		}, DatasetRangeSelectionEvent.TYPE);
@@ -175,14 +194,13 @@ public class TimeSeriesView extends BaseComposite{
 	
 	@UiHandler("randomize")
 	protected void handleRandomize(ClickEvent event) {
-		for (Dataset dataset : chart.getData().getDatasets()){
+		for (Dataset dataset : small.getData().getDatasets()){
 			LineDataset scDataset = (LineDataset)dataset;
 			for (DataPoint dp : scDataset.getDataPoints()){
 				dp.setY(getRandomDigit(false));
 			}
 		}
-		chart.update();
+//		chart.update();
 		small.update();
 	}
-
 }
