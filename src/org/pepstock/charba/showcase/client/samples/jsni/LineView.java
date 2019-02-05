515c64116a1af8106d5116fe8c253a24fd841a08
stock <stocki.nail@gmail.com>
2019-02-05 18:07:13 +0100

    commit issue 22

--- src/org/pepstock/charba/showcase/client/samples/jsni/LineView.java
+++ src/org/pepstock/charba/showcase/client/samples/jsni/LineView.java
@@ -1,134 +1,68 @@
 package org.pepstock.charba.showcase.client.samples.jsni;
 
-import java.util.List;
+import java.util.logging.Logger;
 
 import org.pepstock.charba.client.LineChart;
-import org.pepstock.charba.client.colors.IsColor;
-import org.pepstock.charba.client.data.Dataset;
 import org.pepstock.charba.client.data.LineDataset;
-import org.pepstock.charba.client.enums.Fill;
-import org.pepstock.charba.client.enums.InteractionMode;
-import org.pepstock.charba.client.enums.Position;
-import org.pepstock.charba.client.options.scales.CartesianCategoryAxis;
 import org.pepstock.charba.client.options.scales.CartesianLinearAxis;
-import org.pepstock.charba.showcase.client.samples.Colors;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-
 /**
-
+ * 
  * @author Andrea "Stock" Stocchero
  */
-public class LineView extends BaseComposite{
+public class LineView extends BaseComposite {
 	
+	Logger LOG = Logger.getLogger("mio");
+
 	private static ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
 
 	interface ViewUiBinder extends UiBinder<Widget, LineView> {
 	}
 
 	@UiField
-	LineChart chart;
+	VerticalPanel panel;
 	
+	LineChart chart;
+
 	public LineView() {
 		initWidget(uiBinder.createAndBindUi(this));
-		
+
+		LineChart chart = new LineChart();
+		chart = new LineChart();
+//		chart.setWidth("100%");
+		chart.getOptions().getLayout().getPadding().setTop(16);
+		chart.getOptions().getLayout().getPadding().setBottom(32);
 		chart.getOptions().setResponsive(true);
-		chart.getOptions().setMaintainAspectRatio(true);
-		chart.getOptions().setAspectRatio(3);
-		chart.getOptions().getLegend().setPosition(Position.top);
-		chart.getOptions().getTitle().setDisplay(true);
-		chart.getOptions().getTitle().setText("Charba Line Chart");
-		chart.getOptions().getTooltips().setMode(InteractionMode.index);
-		chart.getOptions().getTooltips().setIntersect(false);
-		chart.getOptions().getHover().setMode(InteractionMode.nearest);
-		chart.getOptions().getHover().setIntersect(true);
+//		chart.getOptions().setMaintainAspectRatio(false);
+		chart.getOptions().getLegend().setDisplay(true);
 		
-		LineDataset dataset1 = chart.newDataset();
-		dataset1.setLabel("dataset 1");
+		LineDataset dataSet = chart.newDataset();
+		dataSet.setBackgroundColor("#4863A0");
+		dataSet.setData(new double[] { 90.0, 80.0, 88.0 });
+		chart.getData().setDatasets(dataSet);
+		chart.getData().setLabels("one", "two", "three");
 		
-		IsColor color1 = Colors.ALL[0];
+		CartesianLinearAxis axis = new CartesianLinearAxis(chart);
+		axis.setDisplay(true);
+		axis.getTicks().setBeginAtZero(true);
+		axis.getScaleLabel().setDisplay(true);
+		axis.getScaleLabel().setLabelString("Value");
+		chart.getOptions().getScales().setYAxes(axis);
 		
-		dataset1.setBackgroundColor(color1.toHex());
-		dataset1.setBorderColor(color1.toHex());
-		double[] values = getRandomDigits(months, false);
-		dataset1.setData(values);
-		dataset1.setFill(Fill.nofill);
-
-		LineDataset dataset2 = chart.newDataset();
-		dataset2.setLabel("dataset 2");
-		
-		IsColor color2 = Colors.ALL[1];
-		
-		dataset2.setBackgroundColor(color2.toHex());
-		dataset2.setBorderColor(color2.toHex());
-		dataset2.setData(getRandomDigits(months, false));
-		dataset2.setFill(Fill.nofill);
-		dataset2.setHidden(false);
-
-		CartesianCategoryAxis axis1 = new CartesianCategoryAxis(chart);
-		axis1.setDisplay(true);
-		axis1.getScaleLabel().setDisplay(true);
-		axis1.getScaleLabel().setLabelString("Month");
-		
-		CartesianLinearAxis axis2 = new CartesianLinearAxis(chart);
-		axis2.setDisplay(true);
-		axis2.getTicks().setBeginAtZero(true);
-		axis2.getScaleLabel().setDisplay(true);
-		axis2.getScaleLabel().setLabelString("Value");
-		
-		chart.getOptions().getScales().setXAxes(axis1);
-		chart.getOptions().getScales().setYAxes(axis2);
-		
-		chart.getData().setLabels(getLabels());
-		chart.getData().setDatasets(dataset1, dataset2);
+		panel.add(chart);
 		
 	}
-	
-	@UiHandler("randomize")
+	@UiHandler("add")
 	protected void handleRandomize(ClickEvent event) {
-
-		for (Dataset dataset : chart.getData().getDatasets()){
-			dataset.setData(getRandomDigits(months, false));
-		}
-		chart.update();
-	}
-
-	@UiHandler("add_dataset")
-	protected void handleAddDataset(ClickEvent event) {
-		List<Dataset> datasets = chart.getData().getDatasets();
-		
-		LineDataset dataset = chart.newDataset();
-		dataset.setLabel("dataset "+(datasets.size()+1));
-		
-		IsColor color = Colors.ALL[datasets.size()]; 
-		dataset.setBackgroundColor(color.toHex());
-		dataset.setBorderColor(color.toHex());
-		dataset.setFill(Fill.nofill);
-		dataset.setData(getRandomDigits(months));
-
-		datasets.add(dataset);
-		
-		chart.update();
-	}
-
-	@UiHandler("remove_dataset")
-	protected void handleRemoveDataset(ClickEvent event) {
-		removeDataset(chart);
-	}
-
-	@UiHandler("add_data")
-	protected void handleAddData(ClickEvent event) {
-		addData(chart);
-	}
-
-	@UiHandler("remove_data")
-	protected void handleRemoveData(ClickEvent event) {
-		removeData(chart);
+		LOG.info(chart.toJSONString());
+		panel.add(chart);
 	}
 }
