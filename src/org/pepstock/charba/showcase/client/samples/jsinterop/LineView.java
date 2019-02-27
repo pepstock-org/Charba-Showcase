585ecdcc34b8ac8dfb8f07a5b83673a8d405df91
stock <stocki.nail@gmail.com>
2019-02-27 19:35:38 +0100

    tested option builder

--- src/org/pepstock/charba/showcase/client/samples/jsinterop/LineView.java
+++ src/org/pepstock/charba/showcase/client/samples/jsinterop/LineView.java
@@ -13,8 +13,8 @@ import org.pepstock.charba.client.enums.InteractionMode;
 import org.pepstock.charba.client.enums.Position;
 import org.pepstock.charba.client.impl.plugins.DatasetsItemsSelector;
 import org.pepstock.charba.client.impl.plugins.DatasetsItemsSelectorOptions;
+import org.pepstock.charba.client.impl.plugins.DatasetsItemsSelectorOptionsBuilder;
 import org.pepstock.charba.client.impl.plugins.enums.Align;
-import org.pepstock.charba.client.impl.plugins.enums.Render;
 import org.pepstock.charba.showcase.client.samples.Colors;
 
 import com.google.gwt.core.client.GWT;
@@ -25,8 +25,8 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
 
-public class LineView extends BaseComposite{
-	
+public class LineView extends BaseComposite {
+
 	private static ViewUiBinder uiBinder = GWT.create(ViewUiBinder.class);
 
 	interface ViewUiBinder extends UiBinder<Widget, LineView> {
@@ -34,10 +34,10 @@ public class LineView extends BaseComposite{
 
 	@UiField
 	LineChart chart;
-	
+
 	public LineView() {
 		initWidget(uiBinder.createAndBindUi(this));
-		
+
 		chart.getOptions().setResponsive(true);
 		chart.getOptions().setMaintainAspectRatio(true);
 		chart.getOptions().getLegend().setPosition(Position.top);
@@ -49,25 +49,25 @@ public class LineView extends BaseComposite{
 		chart.getOptions().getHover().setIntersect(true);
 
 		List<Dataset> datasets = chart.getData().getDatasets(true);
-		
+
 		LineDataset dataset1 = chart.newDataset();
 		dataset1.setLabel("dataset 1");
-		
+
 		IsColor color1 = Colors.ALL[0];
-		
+
 		dataset1.setBackgroundColor(color1.toHex());
 		dataset1.setBorderColor(color1.toHex());
 		dataset1.setFill(false);
 		double[] values = getRandomDigits(months);
 		List<Double> data = dataset1.getData(true);
-		for (int i=0; i<values.length; i++) {
+		for (int i = 0; i < values.length; i++) {
 			data.add(values[i]);
 		}
 		datasets.add(dataset1);
-		
+
 		LineDataset dataset2 = chart.newDataset();
 		dataset2.setLabel("dataset 2");
-		
+
 		IsColor color2 = Colors.ALL[1];
 
 		dataset2.setBackgroundColor(color2.toHex());
@@ -80,37 +80,28 @@ public class LineView extends BaseComposite{
 		axis1.setDisplay(true);
 		axis1.getScaleLabel().setDisplay(true);
 		axis1.getScaleLabel().setLabelString("Month");
-		
+
 		CartesianLinearAxis axis2 = new CartesianLinearAxis(chart);
 		axis2.setDisplay(true);
 		axis2.getScaleLabel().setDisplay(true);
 		axis2.getScaleLabel().setLabelString("Value");
-		
+
 		chart.getOptions().getScales().setXAxes(axis1);
 		chart.getOptions().getScales().setYAxes(axis2);
-		
+
 		chart.getData().setLabels(getLabels());
-		
+
 		DatasetsItemsSelector selector = new DatasetsItemsSelector();
-		DatasetsItemsSelectorOptions pOptions = new DatasetsItemsSelectorOptions();
-		pOptions.setBorderWidth(5);
-		pOptions.setBorderDash(6, 2, 3);
-		pOptions.setFireEventOnClearSelection(true);
-		pOptions.getClearSelection().setDisplay(true);
-		pOptions.getClearSelection().setFontSize(18);
-		pOptions.getClearSelection().setAlign(Align.right_chartArea);
-		pOptions.getClearSelection().setPosition(Position.bottom);
-		pOptions.getClearSelection().setRender(Render.image_label);
-		pOptions.getClearSelection().setUseSelectionStyle(true);
-	
+		DatasetsItemsSelectorOptions pOptions = DatasetsItemsSelectorOptionsBuilder.create().setBorderWidth(5).setBorderDash(6, 2, 3).setFireEventOnClearSelection(true).setDisplay(true).setFontSize(18).setAlign(Align.right_chartArea)
+				.setUseSelectionStyle(true).build();
+
 		chart.getOptions().getPlugins().setOptions(DatasetsItemsSelector.ID, pOptions);
 		chart.getPlugins().add(selector);
 	}
-	
-	
+
 	@UiHandler("randomize")
 	protected void handleRandomize(ClickEvent event) {
-		for (Dataset dataset : chart.getData().getDatasets()){
+		for (Dataset dataset : chart.getData().getDatasets()) {
 			dataset.setData(getRandomDigits(months));
 		}
 		chart.update();
@@ -119,18 +110,18 @@ public class LineView extends BaseComposite{
 	@UiHandler("add_dataset")
 	protected void handleAddDataset(ClickEvent event) {
 		List<Dataset> datasets = chart.getData().getDatasets();
-		
+
 		LineDataset dataset = chart.newDataset();
-		dataset.setLabel("dataset "+(datasets.size()+1));
-		
-		IsColor color = Colors.ALL[datasets.size()]; 
+		dataset.setLabel("dataset " + (datasets.size() + 1));
+
+		IsColor color = Colors.ALL[datasets.size()];
 		dataset.setBackgroundColor(color.toHex());
 		dataset.setBorderColor(color.toHex());
 		dataset.setFill(Fill.nofill);
 		dataset.setData(getRandomDigits(months));
 
 		datasets.add(dataset);
-		
+
 		chart.update();
 	}
 
@@ -143,7 +134,7 @@ public class LineView extends BaseComposite{
 	protected void handleAddData(ClickEvent event) {
 		addData(chart);
 	}
-	
+
 	@UiHandler("remove_data")
 	protected void handleRemoveData(ClickEvent event) {
 		removeData(chart);
